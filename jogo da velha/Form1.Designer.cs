d0f6d47cc4b6fec0169ba611cb490f896bf2b979
Eliseu <eliseudaniel29@gmail.com>
2024-03-12 00:05:23 -0300

    Add project files.

---
+++ jogo da velha/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace jogo_da_velha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblContX = new System.Windows.Forms.Label();
+            this.lblContEmpate = new System.Windows.Forms.Label();
+            this.lblContO = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(56, 216);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(59, 60);
+            this.btn1.TabIndex = 0;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(121, 216);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(59, 60);
+            this.btn2.TabIndex = 1;
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(186, 216);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(59, 60);
+            this.btn3.TabIndex = 2;
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(56, 282);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(59, 60);
+            this.btn4.TabIndex = 3;
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(121, 282);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(59, 60);
+            this.btn5.TabIndex = 4;
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(186, 282);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(59, 60);
+            this.btn6.TabIndex = 5;
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(56, 348);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(59, 60);
+            this.btn7.TabIndex = 6;
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(121, 348);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(59, 60);
+            this.btn8.TabIndex = 7;
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(186, 348);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(59, 60);
+            this.btn9.TabIndex = 8;
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Perpetua Titling MT", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(51, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 28);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "JOGO DA VELHA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(64, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "X";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(210, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "O";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(129, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Empate";
+            // 
+            // lblContX
+            // 
+            this.lblContX.AutoSize = true;
+            this.lblContX.Location = new System.Drawing.Point(65, 160);
+            this.lblContX.Name = "lblContX";
+            this.lblContX.Size = new System.Drawing.Size(13, 13);
+            this.lblContX.TabIndex = 13;
+            this.lblContX.Text = "0";
+            // 
+            // lblContEmpate
+            // 
+            this.lblContEmpate.AutoSize = true;
+            this.lblContEmpate.Location = new System.Drawing.Point(142, 160);
+            this.lblContEmpate.Name = "lblContEmpate";
+            this.lblContEmpate.Size = new System.Drawing.Size(13, 13);
+            this.lblContEmpate.TabIndex = 14;
+            this.lblContEmpate.Text = "0";
+            // 
+            // lblContO
+            // 
+            this.lblContO.AutoSize = true;
+            this.lblContO.Location = new System.Drawing.Point(210, 160);
+            this.lblContO.Name = "lblContO";
+            this.lblContO.Size = new System.Drawing.Size(13, 13);
+            this.lblContO.TabIndex = 15;
+            this.lblContO.Text = "0";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(220, 461);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 16;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 496);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblContO);
+            this.Controls.Add(this.lblContEmpate);
+            this.Controls.Add(this.lblContX);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "JOGO DA VELHA";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblContX;
+        private System.Windows.Forms.Label lblContEmpate;
+        private System.Windows.Forms.Label lblContO;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ jogo da velha/Form1.cs
@@ -0,0 +1,418 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace jogo_da_velha
+{
+    public partial class Form1 : Form
+    {
+        string jogador = "X";
+
+        int jogador1 = 0, jogador2 = 0, empate = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if (btn1.Text == "")
+            {
+                if (jogador == "X")
+                {
+                    btn1.Text = "X";
+                    btn1.BackColor = Color.Red;
+                    jogador = "O";
+                }
+                else
+                {
+                    btn1.Text = "O";
+                    btn1.BackColor = Color.Blue;
+                    jogador = "X";
+
+                }
+            }
+
+            //somando pontos
+            if (btn1.Text == "X")
+            {
+                jogador1 = jogador1++;
+            }
+            else
+            {
+                jogador2 = jogador2++;
+            }
+
+            //conferindo ganhador
+            if (jogador1 == 3)
+            {
+                lblContX.Text = Convert.ToString(jogador1 + 1);
+            }
+            if (jogador2 == 3)
+            {
+                lblContO.Text = Convert.ToString(jogador2);
+            }
+
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            if (btn2.Text == "")
+            {
+                if (jogador == "X")
+                {
+                    btn2.Text = "X";
+                    btn2.BackColor = Color.Red;
+                    jogador = "O";
+                }
+                else
+                {
+                    btn2.Text = "O";
+                    btn2.BackColor = Color.Blue;
+                    jogador = "X";
+
+                }
+            }
+
+            //somando pontos
+            if (btn1.Text == "X")
+            {
+                jogador1 = jogador1++;
+            }
+            else
+            {
+                jogador2 = jogador2++;
+            }
+
+            //conferindo ganhador
+            if (jogador1 == 3)
+            {
+                lblContX.Text = Convert.ToString(jogador1);
+            }
+            if (jogador2 == 3)
+            {
+                lblContO.Text = Convert.ToString(jogador2);
+            }
+
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            if (btn3.Text == "")
+            {
+                if (jogador == "X")
+                {
+                    btn3.Text = "X";
+                    btn3.BackColor = Color.Red;
+                    jogador = "O";
+                }
+                else
+                {
+                    btn3.Text = "O";
+                    btn3.BackColor = Color.Blue;
+                    jogador = "X";
+
+                }
+            }
+
+            //somando pontos
+            if (btn1.Text == "X")
+            {
+                jogador1 = jogador1++;
+            }
+            else
+            {
+                jogador2 = jogador2++;
+            }
+
+            //conferindo ganhador
+            if (jogador1 == 3)
+            {
+                lblContX.Text = Convert.ToString(jogador1);
+            }
+            if (jogador2 == 3)
+            {
+                lblContO.Text = Convert.ToString(jogador2);
+            }
+
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            if (btn4.Text == "")
+            {
+                if (jogador == "X")
+                {
+                    btn4.Text = "X";
+                    btn4.BackColor = Color.Red;
+                    jogador = "O";
+                }
+                else
+                {
+                    btn4.Text = "O";
+                    btn4.BackColor = Color.Blue;
+                    jogador = "X";
+
+                }
+            }
+
+            //somando pontos
+            if (btn1.Text == "X")
+            {
+                jogador1 = jogador1++;
+            }
+            else
+            {
+                jogador2 = jogador2++;
+            }
+
+            //conferindo ganhador
+            if (jogador1 == 3)
+            {
+                lblContX.Text = Convert.ToString(jogador1);
+            }
+            if (jogador2 == 3)
+            {
+                lblContO.Text = Convert.ToString(jogador2);
+            }
+
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            if (btn5.Text == "")
+            {
+                if (jogador == "X")
+                {
+                    btn5.Text = "X";
+                    btn5.BackColor = Color.Red;
+                    jogador = "O";
+                }
+                else
+                {
+                    btn5.Text = "O";
+                    btn5.BackColor = Color.Blue;
+                    jogador = "X";
+
+                }
+            }
+
+            //somando pontos
+            if (btn1.Text == "X")
+            {
+                jogador1 = jogador1++;
+            }
+            else
+            {
+                jogador2 = jogador2++;
+            }
+
+            //conferindo ganhador
+            if (jogador1 == 3)
+            {
+                lblContX.Text = Convert.ToString(jogador1);
+            }
+            if (jogador2 == 3)
+            {
+                lblContO.Text = Convert.ToString(jogador2);
+            }
+
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            if (btn6.Text == "")
+            {
+                if (jogador == "X")
+                {
+                    btn6.Text = "X";
+                    btn6.BackColor = Color.Red;
+                    jogador = "O";
+                }
+                else
+                {
+                    btn6.Text = "O";
+                    btn6.BackColor = Color.Blue;
+                    jogador = "X";
+
+                }
+            }
+
+            //somando pontos
+            if (btn1.Text == "X")
+            {
+                jogador1 = jogador1++;
+            }
+            else
+            {
+                jogador2 = jogador2++;
+            }
+
+            //conferindo ganhador
+            if (jogador1 == 3)
+            {
+                lblContX.Text = Convert.ToString(jogador1);
+            }
+            if (jogador2 == 3)
+            {
+                lblContO.Text = Convert.ToString(jogador2);
+            }
+
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            if (btn7.Text == "")
+            {
+                if (jogador == "X")
+                {
+                    btn7.Text = "X";
+                    btn7.BackColor = Color.Red;
+                    jogador = "O";
+                }
+                else
+                {
+                    btn7.Text = "O";
+                    btn7.BackColor = Color.Blue;
+                    jogador = "X";
+
+                }
+            }
+
+            //somando pontos
+            if (btn1.Text == "X")
+            {
+                jogador1 = jogador1++;
+            }
+            else
+            {
+                jogador2 = jogador2++;
+            }
+
+            //conferindo ganhador
+            if (jogador1 == 3)
+            {
+                lblContX.Text = Convert.ToString(jogador1);
+            }
+            if (jogador2 == 3)
+            {
+                lblContO.Text = Convert.ToString(jogador2);
+            }
+
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            if (btn8.Text == "")
+            {
+                if (jogador == "X")
+                {
+                    btn8.Text = "X";
+                    btn8.BackColor = Color.Red;
+                    jogador = "O";
+                }
+                else
+                {
+                    btn8.Text = "O";
+                    btn8.BackColor = Color.Blue;
+                    jogador = "X";
+
+                }
+            }
+
+            //somando pontos
+            if (btn1.Text == "X")
+            {
+                jogador1 = jogador1++;
+            }
+            else
+            {
+                jogador2 = jogador2++;
+            }
+
+            //conferindo ganhador
+            if (jogador1 == 3)
+            {
+                lblContX.Text = Convert.ToString(jogador1);
+            }
+            if (jogador2 == 3)
+            {
+                lblContO.Text = Convert.ToString(jogador2);
+            }
+
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            if (btn9.Text == "")
+            {
+                if (jogador == "X")
+                {
+                    btn9.Text = "X";
+                    btn9.BackColor = Color.Red;
+                    jogador = "O";
+                }
+                else
+                {
+                    btn9.Text = "O";
+                    btn9.BackColor = Color.Blue;
+                    jogador = "X";
+
+                }
+            }
+
+            //somando pontos
+            if (btn1.Text == "X")
+            {
+                jogador1 = jogador1++;
+            }
+            else
+            {
+                jogador2 = jogador2++;
+            }
+
+            //conferindo ganhador
+            if(jogador1 == 3)
+            {
+                lblContX.Text = Convert.ToString(jogador1);
+            }
+            if(jogador2 == 3)
+            {
+                lblContO.Text = Convert.ToString(jogador2);
+            }
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            btn1.Text =
+            btn2.Text =
+            btn3.Text =
+            btn4.Text =
+            btn5.Text =
+            btn6.Text =
+            btn7.Text =
+            btn8.Text =
+            btn9.Text = string.Empty;
+
+            btn1.BackColor = 
+            btn2.BackColor =
+            btn3.BackColor =
+            btn4.BackColor =
+            btn5.BackColor =
+            btn6.BackColor =
+            btn7.BackColor =
+            btn8.BackColor =
+            btn9.BackColor = DefaultBackColor;
+        }
+    }
+}
